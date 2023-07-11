f7ac959867b180a12d7006fa70c52d56fbd69bcc
Mark Berney <berneydwmark@gmail.com>
2023-07-11 22:03:06 +1200

    restrict to 24H

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -3,32 +3,19 @@ import java.util.Scanner;
 public class ConsoleApp {
     private static Scanner scanner;
 
-    private Boolean is24;
     private int startTime;
     private int endTime;
     private int timeBetween;
 
     private ConsoleApp() {
         scanner = new Scanner(System.in);
-        requestClockType();
+
+        System.out.println("Please use 24H Time! E.G: 18:15");
         requestStartTime();
         requestEndTime();
 
         timeBetween = calcTimeBetween(startTime, endTime);
         System.out.println(timeBetween);
-    }
-
-    private void requestClockType() {
-        System.out.print("Do you want 12H or 24H? ");
-        String clockType = scanner.nextLine();
-
-        is24 = is24H(clockType);
-    }
-
-    private boolean is24H(String input) {
-        if (input.equals("12H"))
-            return false;
-        else return input.equals("24H");
     }
 
     private void requestStartTime() {
