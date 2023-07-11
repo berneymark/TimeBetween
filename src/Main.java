df817dc8e9b5dd5965df4a0c67ef92f6e03b62f9
Mark Berney <berneydwmark@gmail.com>
2023-07-11 13:08:33 +1200

    refactor basic code

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scanner;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public Main() {
+        scanner = new Scanner(System.in);
+        requestStartTime();
+    }
 
+    public String requestStartTime() {
         System.out.print("What time did you start work? ");
         String startTime = scanner.nextLine();
 
         System.out.println("Start time is: " + startTime);
+
+        return startTime;
+    }
+
+    public static void main(String[] args) {
+        Main startProg = new Main();
     }
 }
