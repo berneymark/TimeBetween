4037717cbcef7524d4195aff2719141f5159658e
Mark Berney <berneydwmark@gmail.com>
2023-07-11 12:59:11 +1200

    request and store start time

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What time did you start work? ");
+        String startTime = scanner.nextLine();
+
+        System.out.println("Start time is: " + startTime);
     }
 }
